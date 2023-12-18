3d708b97851bf24a667f15353854bf17403b05b3
Isabella Cabral <bellacabral80@gmail.com>
2023-12-18 15:04:57 -0300

    mod5 testes e documentação

---
+++ Testes_Documentacao_mod5/src/main/Calculadora.java
@@ -0,0 +1,38 @@
+package main;
+
+import test.CalcTest;
+
+/**
+ * @author bella
+ * 
+ * A classe Calculadora foi criada para realizar cálculos simples.
+ * Soma, Subtração, Multiplicação e Divisão
+ * 
+ * @see CalcTest -> Para vizualizar seu funcionamento (testes)
+ *  
+*/
+
+
+public class Calculadora {
+	
+	public int soma(int a, int b) {
+		return a + b;
+	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mult(int a, int b) {
+		return a * b;
+	}
+	
+	public int divi(int a, int b) {
+		if(b != 0) {
+			return a / b;
+		} else {
+			throw new RuntimeException("Erro: O divisor não pode ser zero! ");
+		}
+	}
+
+}
