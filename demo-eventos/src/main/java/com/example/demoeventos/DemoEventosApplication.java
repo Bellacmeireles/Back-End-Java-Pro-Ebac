dbb7e4ac8839c0681d7ba4d57afd615d82e3de1b
Isabella Cabral <bellacabral80@gmail.com>
2024-01-12 17:25:59 -0300

    demo-eventos

---
+++ demo-eventos/src/main/java/com/example/demoeventos/DemoEventosApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEventosApplication.class, args);
+	}
+
+}
---
+++ demo-eventos/src/test/java/com/example/demoeventos/DemoEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
