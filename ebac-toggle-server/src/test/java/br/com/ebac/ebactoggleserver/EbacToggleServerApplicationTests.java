147a8f582c06494be515d495adaa7d657b98ad16
Isabella Cabral <bellacabral80@gmail.com>
2024-01-17 21:35:28 -0300

    Primeiro coommit

---
+++ ebac-toggle-server/src/test/java/br/com/ebac/ebactoggleserver/EbacToggleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ebac.ebactoggleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbacToggleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
